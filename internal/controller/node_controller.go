641c3978470287a73f72d4cf3b851a9bb33c590a
Fabian Wiesel <fabian.wiesel@sap.com>
2024-11-13 14:09:56 +0100

    NodeController: Reduce updates
    By using a label filter and only the metadata of the node,
    we reduce the load on the k8s api for unneeded calls.

--- internal/controller/node_controller.go
+++ internal/controller/node_controller.go
@@ -34,8 +34,10 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 	"sigs.k8s.io/controller-runtime/pkg/cluster"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	logger "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 
 	kvmv1 "github.com/cobaltcore-dev/openstack-hypervisor-operator/api/v1"
@@ -47,19 +49,54 @@ const (
 	MAINTENANCE_REQUIRED_LABEL = "cloud.sap/maintenance-required"
 	MAINTENANCE_APPROVED_LABEL = "cloud.sap/maintenance-approved"
 	HOST_LABEL                 = "kubernetes.metal.cloud.sap/host"
+	MANAGED_BY                 = "app.kubernetes.io/managed-by"
+	MANAGER_NAME               = "openstack-node-controller"
+	// Changing MANAGER_NAME will cause the app to lose track of already
+	// created Evictions by this controller
 )
 
 type NodeReconciler struct {
 	serviceClient *gophercloud.ServiceClient
 }
 
+type NodeMetadata = metav1.PartialObjectMetadata
+
+type nodeControllerRequest struct {
+	kind           string
+	namespacedName types.NamespacedName
+	clusterName    string
+	client         k8sclient.Client
+	state          string
+}
+
 // +kubebuilder:rbac:groups="",resources=nodes,verbs=get;list;watch;patch
+// +kubebuilder:rbac:groups=kvm.cloud.sap,resources=evictions,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-func (r *NodeReconciler) Reconcile(ctx context.Context, req request) (ctrl.Result, error) {
+func (r *NodeReconciler) Reconcile(ctx context.Context, req nodeControllerRequest) (ctrl.Result, error) {
+	log := logger.FromContext(ctx)
+	log = log.WithName("Reconcile").WithName(req.kind)
+	ctx = logger.IntoContext(ctx, log)
+
+	switch req.kind {
+	case "Node":
+		return r.reconcileNode(ctx, req)
+	case "Eviction":
+		return r.reconcileEviction(ctx, req)
+	default:
+		log.Info("Got reconciliation request for unknown kind", "kind", req.kind)
+		return ctrl.Result{}, nil
+	}
+}
+
+func (r *NodeReconciler) reconcileNode(ctx context.Context, req nodeControllerRequest) (ctrl.Result, error) {
 	node := &corev1.Node{}
-	if err := req.client.Get(ctx, req.NamespacedName, node); err != nil {
+	log := logger.FromContext(ctx)
+	log = log.WithValues("node", req.namespacedName.Name)
+	ctx = logger.IntoContext(ctx, log)
+
+	if err := req.client.Get(ctx, req.namespacedName, node); err != nil {
 		// ignore not found errors, could be deleted
 		return ctrl.Result{}, k8sclient.IgnoreNotFound(err)
 	}
@@ -73,8 +110,8 @@ func (r *NodeReconciler) Reconcile(ctx context.Context, req request) (ctrl.Resul
 		return ctrl.Result{Requeue: true}, nil
 	}
 
-	changed, err = r.reconcileEviction(ctx, req.client, node, host)
-	return ctrl.Result{Requeue: changed}, err
+	err = r.reconcileEvictionForNode(ctx, req.client, node, host, req.state)
+	return ctrl.Result{}, err
 }
 
 // normalizeName returns the host name of the node. If the host name is not set, it will be set to the node name.
@@ -131,43 +168,59 @@ func (r *NodeReconciler) normalizeName(ctx context.Context, client k8sclient.Cli
 	return host, changed, nil
 }
 
-// reconcileEviction ensures that an eviction is created if the node has the maintenance label.
-func (r *NodeReconciler) reconcileEviction(ctx context.Context, client k8sclient.Client, node *corev1.Node, host string) (bool, error) {
-	requiredValue, requiredFound := node.Labels[MAINTENANCE_REQUIRED_LABEL]
-	_, approvedFound := node.Labels[MAINTENANCE_APPROVED_LABEL]
-
+// reconcileEvictionForNode ensures that an eviction is created if the node has the maintenance label.
+func (r *NodeReconciler) reconcileEvictionForNode(ctx context.Context, client k8sclient.Client, node *corev1.Node, host, state string) error {
 	name := fmt.Sprintf("maintenance-required-%v", host)
 	eviction := &kvmv1.Eviction{ObjectMeta: metav1.ObjectMeta{
 		Name:      name,
 		Namespace: "monsoon3", // todo: change to the correct namespace or use cluster scoped CRs
+		Labels:    map[string]string{MANAGED_BY: MANAGER_NAME},
 	}}
 
-	if !requiredFound {
-		if !approvedFound {
-			return false, k8sclient.IgnoreNotFound(client.Delete(ctx, eviction))
-		} else {
-			return false, nil
-		}
+	log := logger.FromContext(ctx)
+
+	if state == "" {
+		log.Info("no label", "eviction", eviction)
+		return k8sclient.IgnoreNotFound(client.Delete(ctx, eviction))
 	}
 
+	log.Info("with label", "eviction", eviction)
 	_, err := controllerutil.CreateOrUpdate(ctx, client, eviction, func() error {
+		eviction.Labels[MANAGED_BY] = MANAGER_NAME
 		eviction.Spec.Hypervisor = node.Name
-		eviction.Spec.Reason = fmt.Sprintf("Node %v, label %v=%v", node.Name, MAINTENANCE_REQUIRED_LABEL, requiredValue)
+		eviction.Spec.Reason = fmt.Sprintf("Node %v, label %v=%v", node.Name, MAINTENANCE_REQUIRED_LABEL, state)
 		return nil
 	})
+	return err
+}
 
-	if err != nil {
-		return false, err
+// reconcileEviction ensures that an eviction is created if the node has the maintenance label.
+func (r *NodeReconciler) reconcileEviction(ctx context.Context, req nodeControllerRequest) (ctrl.Result, error) {
+	var eviction kvmv1.Eviction
+	if err := req.client.Get(ctx, req.namespacedName, &eviction); err != nil {
+		return ctrl.Result{}, k8sclient.IgnoreNotFound(err)
+	}
+
+	log := logger.FromContext(ctx)
+	log = log.WithValues("eviction", eviction.Name)
+	ctx = logger.IntoContext(ctx, log)
+
+	node := &corev1.Node{}
+	if err := req.client.Get(ctx, types.NamespacedName{Name: eviction.Spec.Hypervisor}, node); err != nil {
+		return ctrl.Result{}, k8sclient.IgnoreNotFound(err)
 	}
 
 	switch eviction.Status.EvictionState {
 	case "Succeeded":
-		return r.setNodeLabels(ctx, client, node, map[string]string{MAINTENANCE_APPROVED_LABEL: "true"})
+		_, err := r.setNodeLabels(ctx, req.client, node, map[string]string{MAINTENANCE_APPROVED_LABEL: "true"})
+		return ctrl.Result{}, err
 	case "Failed":
-		return r.setNodeLabels(ctx, client, node, map[string]string{MAINTENANCE_APPROVED_LABEL: "false"})
+		_, err := r.setNodeLabels(ctx, req.client, node, map[string]string{MAINTENANCE_APPROVED_LABEL: "false"})
+		return ctrl.Result{}, err
+	default:
+		// We will also be called for the old state (i.e. Running) and simply ignore that
+		return ctrl.Result{}, nil
 	}
-
-	return false, nil
 }
 
 // setHostLabel sets the host label on the node.
@@ -206,10 +259,10 @@ func (r *NodeReconciler) SetupWithManagerAndClusters(mgr ctrl.Manager, clusters 
 		r.serviceClient.ResourceBase = r.serviceClient.Endpoint + "v2.0/"
 	}
 
-	b := builder.TypedControllerManagedBy[request](mgr).
-		Named("openstack-node-controller")
+	b := builder.TypedControllerManagedBy[nodeControllerRequest](mgr).
+		Named(MANAGER_NAME)
 
-	metaObj := &metav1.PartialObjectMetadata{}
+	metaObj := &NodeMetadata{}
 	gvk, err := apiutil.GVKForObject(&corev1.Node{}, mgr.GetScheme())
 	if err != nil {
 		return fmt.Errorf("unable to determine GVK of corev1.Node for a metadata-only watch: %w", err)
@@ -220,14 +273,62 @@ func (r *NodeReconciler) SetupWithManagerAndClusters(mgr ctrl.Manager, clusters 
 		b = b.WatchesRawSource(source.TypedKind(
 			cluster.GetCache(),
 			metaObj,
-			handler.TypedEnqueueRequestsFromMapFunc(func(ctx context.Context, n *metav1.PartialObjectMetadata) []request {
-				return []request{{
-					NamespacedName: types.NamespacedName{Namespace: n.Namespace, Name: n.Name},
+			handler.TypedEnqueueRequestsFromMapFunc(func(ctx context.Context, n *NodeMetadata) []nodeControllerRequest {
+				// This will be called twice: once for the old and once for the new value (unfortunately)
+				return []nodeControllerRequest{{
+					kind:           "Node",
+					namespacedName: types.NamespacedName{Namespace: n.Namespace, Name: n.Name},
 					clusterName:    clusterName,
 					client:         cluster.GetClient(),
+					state:          n.Labels[MAINTENANCE_REQUIRED_LABEL],
 				}}
 			}),
+			predicate.TypedFuncs[*NodeMetadata]{
+				UpdateFunc: func(e event.TypedUpdateEvent[*NodeMetadata]) bool {
+					newValue := e.ObjectNew.Labels[MAINTENANCE_REQUIRED_LABEL]
+					oldValue := e.ObjectOld.Labels[MAINTENANCE_REQUIRED_LABEL]
+
+					return newValue != oldValue
+				},
+				DeleteFunc:  func(e event.TypedDeleteEvent[*NodeMetadata]) bool { return false },
+				GenericFunc: func(e event.TypedGenericEvent[*NodeMetadata]) bool { return false },
+			},
 		))
+
+		b = b.WatchesRawSource(source.TypedKind(
+			cluster.GetCache(),
+			&kvmv1.Eviction{},
+			handler.TypedEnqueueRequestsFromMapFunc(func(ctx context.Context, e *kvmv1.Eviction) []nodeControllerRequest {
+				// This will be called twice: once for the old and once for the new value (unfortunately)
+				return []nodeControllerRequest{{
+					kind:           "Eviction",
+					namespacedName: types.NamespacedName{Namespace: e.Namespace, Name: e.Name},
+					clusterName:    clusterName,
+					client:         cluster.GetClient(),
+					state:          e.Status.EvictionState,
+				}}
+			}),
+			predicate.TypedFuncs[*kvmv1.Eviction]{
+				CreateFunc: func(e event.TypedCreateEvent[*kvmv1.Eviction]) bool { return false },
+				UpdateFunc: func(e event.TypedUpdateEvent[*kvmv1.Eviction]) bool {
+					if value, found := e.ObjectNew.Labels[MANAGED_BY]; !found || value != MANAGER_NAME {
+						return false
+					}
+					newEvictionState := e.ObjectNew.Status.EvictionState
+					if newEvictionState == e.ObjectOld.Status.EvictionState {
+						// Only changes
+						return false
+					}
+					if newEvictionState != "Failed" && newEvictionState != "Succeeded" {
+						// And only the final states
+						return false
+					}
+					return true
+				},
+				DeleteFunc:  func(e event.TypedDeleteEvent[*kvmv1.Eviction]) bool { return false },
+				GenericFunc: func(e event.TypedGenericEvent[*kvmv1.Eviction]) bool { return false },
+			}),
+		)
 	}
 
 	return b.Complete(r)
