f07ccd1526442ae787c92c6d58580b6c9b259a17
Fabian Wiesel <fabian.wiesel@sap.com>
2025-03-06 16:29:44 +0100

    Added traits controller syncing annotations

--- internal/controller/utils.go
+++ internal/controller/utils.go
@@ -80,3 +80,14 @@ func hasAnyLabel(labels map[string]string, list ...string) bool {
 
 	return false
 }
+
+// setNodeAnnotations sets annotations on the node.
+func setNodeAnnotations(ctx context.Context, writer client.Writer, node *corev1.Node, annotations map[string]string) (bool, error) {
+	newNode := node.DeepCopy()
+	maps.Copy(newNode.Annotations, annotations)
+	if maps.Equal(node.Annotations, newNode.Annotations) {
+		return false, nil
+	}
+
+	return true, writer.Patch(ctx, newNode, client.MergeFrom(node))
+}
