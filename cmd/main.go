84f495c1bcdfd41ec11f8a9319f1825c0a6de0ef
Fabian Wiesel <fabian.wiesel@sap.com>
2025-11-13 14:07:48 +0100

    HypervisorMaintenanceController: Enable/Disable compute service
    The controller only gets active after onboarding, as that one needs
    to take care of enabling and aggregate association depending on the
    tests.
    
    If maintenance is set, it will disable now the compute host in nova,
    and enable it, if it is unset.
    It will only do so on an "edge", i.e. if it hasn't done it before.

--- cmd/main.go
+++ cmd/main.go
@@ -213,6 +213,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controller.HypervisorMaintenanceController{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", controller.HypervisorMaintenanceControllerName)
+		os.Exit(1)
+	}
+
 	if err = (&controller.EvictionReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
