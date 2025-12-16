114aef1f3051b05b9aa4a37fe90e9c577e0f76cd
Fabian Wiesel <fabian.wiesel@sap.com>
2025-12-16 17:23:54 +0100

    transportLabels for arbitrary metadata
    This opens the way to transport the labels between more resource types.

--- internal/controller/hypervisor_maintenance_controller.go
+++ internal/controller/hypervisor_maintenance_controller.go
@@ -233,7 +233,7 @@ func (hec *HypervisorMaintenanceController) ensureEviction(ctx context.Context, 
 		}
 
 		// This also transports the label-selector, if set
-		transportLabels(&eviction.ObjectMeta, hypervisor)
+		transportLabels(&eviction.ObjectMeta, &hypervisor.ObjectMeta)
 
 		if err = hec.Create(ctx, eviction); err != nil {
 			return metav1.ConditionUnknown, fmt.Errorf("failed to create eviction due to %w", err)
